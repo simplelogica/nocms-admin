17f16ab835b31edd171091eae06036bcec8d8239
David J. Brenes <david.brenes@simplelogica.net>
2014-03-11 10:28:47 +0100

    When declaring routes in the menu items we beed to be able to handle procs
    That's because routes may not have been loaded yet and we need to evaluate them later

--- app/helpers/no_cms/admin/application_helper.rb
+++ app/helpers/no_cms/admin/application_helper.rb
@@ -1,6 +1,13 @@
 module NoCms
   module Admin
     module ApplicationHelper
+
+      def menu_item_url menu_item
+        menu_item[:url].is_a?(Proc) ?
+          menu_item[:url].call :
+          menu_item[:url]
+      end
+
     end
   end
 end
