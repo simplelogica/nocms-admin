40f5149d3d5b0fec43dbb811382f4c2636c70968
David J. Brenes <david.brenes@simplelogica.net>
2014-03-12 18:33:40 +0100

    Logging messages in view

--- app/controllers/no_cms/admin/application_controller.rb
+++ app/controllers/no_cms/admin/application_controller.rb
@@ -1,6 +1,12 @@
 module NoCms
   module Admin
     class ApplicationController < ActionController::Base
+
+      before_filter :initialize_logger
+
+      def initialize_logger
+        @logger = NoCms::Admin::Logger.new request
+      end
     end
   end
 end
