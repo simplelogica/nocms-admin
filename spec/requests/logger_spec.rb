229dd91e5715f31ba53c536470aa2d56b4087803
David J. Brenes <david.brenes@simplelogica.net>
2014-03-13 10:29:49 +0100

    Spec for logging
    We create a controller to simulate some logging and test that it really shows

---
+++ spec/requests/logger_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe NoCms::Admin::Logger do
+
+  context "when actions log for now" do
+
+    before do
+      visit new_lorem_ipsum_path
+    end
+
+    subject { page }
+
+    it("should show message in info area") {
+      expect(subject).to have_selector('.content .msg.info', text: 'info message now')
+    }
+
+  end
+
+  context "when actions log for later" do
+
+    before do
+      visit new_lorem_ipsum_path later: 1
+    end
+
+    subject { page }
+
+    it("should show message in info area") {
+      expect(subject).to have_selector('.content .msg.info', text: 'info message later')
+    }
+
+  end
+
+end
