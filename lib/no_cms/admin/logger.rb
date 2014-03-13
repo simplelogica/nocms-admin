1431f6ca9ed08e0cb0043a90be100b054ff458e7
David J. Brenes <david.brenes@simplelogica.net>
2014-03-13 10:28:36 +0100

    Including ability to log for next requests
    We implement this through request.flash

--- lib/no_cms/admin/logger.rb
+++ lib/no_cms/admin/logger.rb
@@ -5,25 +5,34 @@ module NoCms
       # We will need the request if we want to save the messages in flash
       def initialize request
         @request = request
-        @messages = {}
+        request.flash[:no_cms_admin_logger] ||= {}
+        @messages = request.flash[:no_cms_admin_logger].dup
       end
 
-      def info message
-        @messages[:info] ||= []
-        @messages[:info] << message
+      def messages_container later=false
+        later ? @request.flash[:no_cms_admin_logger] : @messages
       end
 
-      def warning message
-        @messages[:warning] ||= []
-        @messages[:warning] << message
+      def info message, later=false
+        container = messages_container later
+        container[:info] ||= []
+        container[:info] << message
       end
 
-      def error message
-        @messages[:error] ||= []
-        @messages[:error] << message
+      def warning message, later=false
+        container = messages_container later
+        container[:warning] ||= []
+        container[:warning] << message
+      end
+
+      def error message, later=false
+        container = messages_container later
+        container[:error] ||= []
+        container[:error] << message
       end
 
       def messages types
+
         @messages.select { |type, _| types.include? type }
       end
 
