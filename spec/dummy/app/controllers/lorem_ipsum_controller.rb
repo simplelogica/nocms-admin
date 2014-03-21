a6a94592210458042701608073616d52b288fbe8
David J. Brenes <david.brenes@simplelogica.net>
2014-03-21 13:43:04 +0100

    Fixing a controller spec

--- spec/dummy/app/controllers/lorem_ipsum_controller.rb
+++ spec/dummy/app/controllers/lorem_ipsum_controller.rb
@@ -1,8 +1,8 @@
 class LoremIpsumController < NoCms::Admin::ApplicationController
 
   def new
-    @logger.info('info message now') if params[:later].nil?
-    @logger.info('info message later', true) unless params[:later].nil?
+    @nocms_logger.info('info message now') if params[:later].nil?
+    @nocms_logger.info('info message later', true) unless params[:later].nil?
     redirect_to lorem_ipsum_index_path unless params[:later].nil?
   end
 
