b59de5f740d5a23e9e94c03e665f8b5e03b4a970
David J. Brenes <david.brenes@simplelogica.net>
2014-06-26 17:04:42 +0200

    Rewriting the map to allow more than one map

--- app/assets/javascripts/no_cms/admin/no_cms_admin.map.js
+++ app/assets/javascripts/no_cms/admin/no_cms_admin.map.js
@@ -12,33 +12,48 @@ NoCMS.Admin.Map = {
 
   // Here will cache the DOM elements
   'DOM': { },
+  async_init: function() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=NoCMS.Admin.Map.init";
+    document.body.appendChild(script);
+  },
 
   // Initilizing elements
   init: function () {
 
-    var center = new google.maps.LatLng("40.4191486", "-3.7031865");
+    var that = this;
 
-    $('.address_map').each(function(i, address_map) {
-      var map = new google.maps.Map(address_map, {
-        center: center,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-
-      var marker = new google.maps.Marker({
-        position:  center,
-        icon: "/assets/no_cms/admin/map-marker.png",
-        draggable: true,
-        map: map
-      });
-
-
-      google.maps.event.addListener(marker, "dragend", function() {
-        $(address_map).siblings('.latitude').val(marker.getPosition().lat());
-        $(address_map).siblings('.longitude').val(marker.getPosition().lng());
-      });
+    $('.address_map').each(function(){
+      that.loadMap(this);
     })
 
   },
+
+  loadMap: function(address_map) {
+
+    var center = new google.maps.LatLng("40.4191486", "-3.7031865");
+
+    var map = new google.maps.Map(address_map, {
+      center: center,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var marker = new google.maps.Marker({
+      position:  center,
+      icon: "/assets/no_cms/admin/map-marker.png",
+      draggable: true,
+      map: map
+    });
+
+    google.maps.event.addListener(marker, "dragend", this.saveCoords);
+
+  },
+  saveCoords: function(e) {
+    $(this.getMap().getDiv()).siblings('.latitude').val(this.getPosition().lat());
+    $(this.getMap().getDiv()).siblings('.longitude').val(this.getPosition().lng());
+  }
+
 
 };
